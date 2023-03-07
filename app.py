b1f6b5499f26cb1eb77e2dbcf4c2babe1a28a938
rajeshpatil88 <rajeshpatil6981@gmail.com>
2023-03-07 12:07:55 +0530

    changes made in app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ li = [ 1, 2 ,3 ,4 ,5]
 
 squares = [ i**2 for i in li]
 cube  = [j**3 for j in li]
-print(f"the squares of the elemets are {squares}")
-print(f"the cubes of the elemets are {cube}")
+print("the squares of the elemets are" ,squares)
+print("the cubes of the elemets are" ,cube)
